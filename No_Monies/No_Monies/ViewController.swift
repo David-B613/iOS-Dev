48202c776d06df3a3ae81960dd8b86c2e8892968
David <60449471+David-B613@users.noreply.github.com>
2020-08-10 18:36:48 -0400

    Add files via upload

---
+++ No_Monies/No_Monies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  No_Monies
+//
+//  Created by David Babers  on 8/2/20.
+//  Copyright © 2020 David Babers . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
